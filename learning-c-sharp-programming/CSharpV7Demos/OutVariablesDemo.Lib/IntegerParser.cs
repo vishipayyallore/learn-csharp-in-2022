1ee09452a2182acb264fd52ce958204438a2a406
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-12-25 00:00:46 +0530

    Today's work

---
+++ learning-c-sharp-programming/CSharpV7Demos/OutVariablesDemo.Lib/IntegerParser.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace OutVariablesDemo.Lib
+{
+
+    public class IntegerParser : IIntegerParser
+    {
+
+        public void IntTryParseV1(string input)
+        {
+            int output;
+            if (int.TryParse(input, out output))
+            {
+                WriteLine($"Input: {input}, Output: {output}");
+            }
+            else
+            {
+                WriteLine($"Could not parse {input}, Output: {output}");
+            }
+        }
+
+        public void IntTryParseV2(string input)
+        {
+            if (int.TryParse(input, out var output))
+            {
+                WriteLine($"Input: {input}, Output: {output}");
+            }
+            else
+            {
+                WriteLine($"Could not parse {input}, Output: {output}");
+            }
+        }
+
+    }
+
+}
---
+++ learning-c-sharp-programming/CSharpV7Demos/OutVariablesDemo.Run/OutVariablesDemoApp.cs
@@ -0,0 +1,34 @@
+﻿using OutVariablesDemo.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutVariablesDemo.Run
+{
+    public class OutVariablesDemoApp
+    {
+
+        private readonly IIntegerParser _integerParser;
+
+        public OutVariablesDemoApp(IIntegerParser integerParser)
+        {
+            _integerParser = integerParser ?? throw new ArgumentNullException(nameof(integerParser));
+        }
+
+        public void Run()
+        {
+            var value1 = "125";
+            var value2 = "A125";
+
+            _integerParser.IntTryParseV1(value1);
+            _integerParser.IntTryParseV1(value2);
+            
+            _integerParser.IntTryParseV2(value1);
+            _integerParser.IntTryParseV2(value2);
+        }
+
+    }
+
+}
