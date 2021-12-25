a40be5a35e402b2d3f59a207fb782f620ae27a9f
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2021-12-25 20:59:44 +0530

    Executed the Program

--- learning-c-sharp-programming/CSharpV2Demos/ArrayDemo.Run/Program.cs
+++ learning-c-sharp-programming/CSharpV2Demos/ArrayDemo.Run/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArrayDemo.Lib;
+using ArrayDemo.Lib.Interfaces;
+using ArrayDemo.Run;
+using Microsoft.Extensions.DependencyInjection;
+using NameGenerator.Lib.Interfaces;
+using static System.Console;
+
+// create service collection
+var services = ConfigureServices();
+
+// Generate a provider
+var serviceProvider = services.BuildServiceProvider();
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+serviceProvider.GetService<ArrayDemoApp>().Run();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
+
+
+static IServiceCollection ConfigureServices()
+{
+    IServiceCollection services = new ServiceCollection();
+
+    // Register the Services
+    services.AddTransient<INameGenerator, NameGenerator.Lib.NameGenerator>();
+    services.AddTransient<INamesArray, NamesArray>();
+    services.AddTransient<IPrintHelper, PrintHelper>();
+
+    // IMPORTANT! Register the application entry point
+    services.AddTransient<ArrayDemoApp>();
+
+    return services;
+}
