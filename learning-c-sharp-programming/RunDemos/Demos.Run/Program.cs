0204614ad611e4bd354511bd73c378a3ef05e289
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-12-26 11:21:39 +0530

    ReOrganized the code

--- learning-c-sharp-programming/RunDemos/Demos.Run/Program.cs
+++ learning-c-sharp-programming/RunDemos/Demos.Run/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demos.Run.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+using OutVariablesDemo.Lib.Sample;
+using static System.Console;
+
+
+// Generate a provider
+var serviceProvider = ConfigureServices();
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+serviceProvider.GetService<OutVariablesDemoApp>().Run();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
+
+static ServiceProvider ConfigureServices()
+{
+    // create service collection
+    IServiceCollection services = new ServiceCollection();
+
+    services.ConfigureOutVariablesServices();
+
+    return services.BuildServiceProvider();
+}
