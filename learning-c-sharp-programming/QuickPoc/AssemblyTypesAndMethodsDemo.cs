9c9581a71a2e9117943d50295c53ba824746fbde
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-01-12 23:15:05 +0530

    Today's work

---
+++ learning-c-sharp-programming/QuickPoc/AssemblyTypesAndMethodsDemo.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Reflection;
+using static System.Console;
+
+namespace QuickPoc
+{
+    public class AssemblyTypesAndMethodsDemo
+    {
+        DataSet? _ds;
+        HttpClient? _httpClient;
+
+        public static void ShowDemo()
+        {
+            Assembly? _assembly = Assembly.GetEntryAssembly();
+            if (_assembly == null)
+            {
+                return;
+            }
+
+
+            // loop through the assemblies that this app references
+            foreach (AssemblyName assemblyName in _assembly.GetReferencedAssemblies())
+            {
+                Assembly? assembly = Assembly.Load(assemblyName);
+
+                int methodCount = 0;
+
+                foreach (TypeInfo typeInfo in assembly.GetTypes())
+                {
+                    methodCount += typeInfo.GetMethods().Count();
+                }
+
+                // output the count of types and their methods
+                WriteLine(
+                   "{0:N0} types with {1:N0} methods in {2} assembly.",
+                   arg0: assembly.DefinedTypes.Count(),
+                   arg1: methodCount, arg2: assemblyName.Name);
+            }
+
+        }
+
+    }
+
+}
---
+++ learning-c-sharp-programming/QuickPoc/NamesWithArrayList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+using static System.Console;
+
+namespace QuickPoc
+{
+    public class NamesWithArrayList
+    {
+        public static void ShowDemo()
+        {
+            ArrayList? names = GenerateNames();
+            PrintNames(names);
+        }
+
+        private static ArrayList GenerateNames()
+        {
+            ArrayList? names = new();
+
+            names.Add("Name 1");
+            names.Add("Name 2");
+            names.Add("Name 3");
+            names.Add("Name 4");
+
+            return names;
+        }
+
+        private static void PrintNames(ArrayList names)
+        {
+            foreach (string name in names)
+            {
+                WriteLine(name);
+            }
+        }
+    }
+}
