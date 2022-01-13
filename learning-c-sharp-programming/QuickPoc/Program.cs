b4166408c18706ab9acd527bc34911244137ed1d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-01-13 23:14:34 +0530

    Today's work

--- learning-c-sharp-programming/QuickPoc/Program.cs
+++ learning-c-sharp-programming/QuickPoc/Program.cs
@@ -1,5 +1,11 @@
 ﻿using QuickPoc;
 
+using static System.Console;
+
 AssemblyTypesAndMethodsDemo.ShowDemo();
 
 NamesWithArrayList.ShowDemo();
+
+BasicDataTypesRangeDemo.ShowDemo();
+
+WriteLine("\n\n Press any key ...");
