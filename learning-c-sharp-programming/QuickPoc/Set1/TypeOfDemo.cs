ced5ef67a263a921557ac46e0a84c54b51ab23ae
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-01-24 13:56:09 +0530

    Update TypeOfDemo.cs

--- learning-c-sharp-programming/QuickPoc/Set1/TypeOfDemo.cs
+++ learning-c-sharp-programming/QuickPoc/Set1/TypeOfDemo.cs
@@ -1,17 +1,41 @@
-﻿namespace QuickPoc.Set1
+﻿using System.Globalization;
+
+namespace QuickPoc.Set1
 {
     public class TypeOfDemo
     {
         public static void ShowDemo()
         {
-            PrintType<string>();
+
+            PrintType<decimal>();
+            List<decimal> prices = new() { 1.5m, 2.3m };
+            PrintItems(prices);
+
+            PrintType<float>();
+            List<float> sales = new() { 11.55f, 22.33f };
+            PrintItems(sales);
+
             PrintType<int>();
+            List<int> rollNumbers = new() { 1, 2 };
+            PrintItems(rollNumbers);
+
+            PrintType<string>();
         }
 
         private static void PrintType<T>()
         {
             Console.WriteLine("typeof(T) = {0}", typeof(T));
             Console.WriteLine("typeof(List<T>) = {0}", typeof(List<T>));
+        }
+
+        private static void PrintItems<T>(List<T> items) where T : IFormattable
+        {
+            CultureInfo culture = CultureInfo.InvariantCulture;
+
+            foreach (T item in items)
+            {
+                Console.WriteLine(item.ToString(null, culture));
+            }
         }
 
     }
