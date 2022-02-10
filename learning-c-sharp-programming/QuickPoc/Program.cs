3af0051bbac27a066b4aef3bde2589d61bc02cc0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-02-10 23:29:01 +0530

    Update Program.cs

--- learning-c-sharp-programming/QuickPoc/Program.cs
+++ learning-c-sharp-programming/QuickPoc/Program.cs
@@ -4,52 +4,44 @@ using QuickPoc.Set1;
 using QuickPoc.Set2;
 using static System.Console;
 
-IHeader header = new Header();
-IFooter footer = new Footer();
+IHeader _header = new Header();
+IFooter _footer = new Footer();
 
 AssemblyTypesAndMethodsDemo.ShowDemo();
 BasicDataTypesRangeDemo.ShowDemo();
 
-header.DisplayHeader('=', "Names With ArrayList");
+_header.DisplayHeader('=', "Names With ArrayList");
 NamesWithArrayList.ShowDemo();
-footer.DisplayFooter('-');
+_footer.DisplayFooter('-');
 
-header.DisplayHeader('=', "Names With String Collection");
+_header.DisplayHeader('=', "Names With String Collection");
 ForegroundColor = ConsoleColor.Cyan;
 NamesWithStringCollection.ShowDemo();
 ForegroundColor = ConsoleColor.White;
-footer.DisplayFooter('-');
+_footer.DisplayFooter('-');
 
-header.DisplayHeader('=', "Names With List");
+_header.DisplayHeader('=', "Names With List");
 ForegroundColor = ConsoleColor.Cyan;
 NamesWithList.ShowDemo();
 ForegroundColor = ConsoleColor.White;
-footer.DisplayFooter('-');
+_footer.DisplayFooter('-');
 
-header.DisplayHeader('=', "Generic Method Demo");
+_header.DisplayHeader('=', "Generic Method Demo");
 ForegroundColor = ConsoleColor.Cyan;
 GenericMethodDemo.ShowDemo();
 ForegroundColor = ConsoleColor.White;
-footer.DisplayFooter('-');
+_footer.DisplayFooter('-');
 
-header.DisplayHeader('=', "TypeOf() Demo", foregroundColor: ConsoleColor.Yellow);
+_header.DisplayHeader('=', "TypeOf() Demo", foregroundColor: ConsoleColor.Yellow);
 ForegroundColor = ConsoleColor.Cyan;
 TypeOfDemo.ShowDemo();
 ForegroundColor = ConsoleColor.White;
-footer.DisplayFooter('-');
+_footer.DisplayFooter('-');
 
-header.DisplayHeader('=', "Nullable Boxing Demo", foregroundColor: ConsoleColor.Yellow);
+_header.DisplayHeader('=', "Nullable Boxing Demo", foregroundColor: ConsoleColor.Yellow);
 ForegroundColor = ConsoleColor.Cyan;
 NullableBoxingDemo.ShowDemo();
 ResetColor();
-footer.DisplayFooter('-');
-
-// Test Run
-//IGenerateName _nameGenerator = new GenerateName();
-//for (int i = 0; i < 1_00_00_000; i++)
-//{
-//    Console.WriteLine(_nameGenerator.GetName());
-//}
-
+_footer.DisplayFooter('-');
 
 WriteLine("\n\n Press any key ...");
