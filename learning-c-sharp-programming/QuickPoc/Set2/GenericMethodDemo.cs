47cdc30c4d53c82a106ec76d038c05e224035def
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-24 12:42:51 +0530

    Code Refactoring

--- learning-c-sharp-programming/QuickPoc/Set2/GenericMethodDemo.cs
+++ learning-c-sharp-programming/QuickPoc/Set2/GenericMethodDemo.cs
@@ -9,22 +9,34 @@ namespace QuickPoc.Set2
         public void ShowDemo()
         {
             List<int> numbersList = new() { 5, 10, 15, 20, 25 };
+            List<string> namesList = new() { "Sri Varu", "Sri", "Maha Vishnu", "Apple", "Ant" };
+            List<bool> boolList = new() { true, false, true, false, true };
 
-            List<int> firstTwoNumbers = CopyAtMost(numbersList, 2);
-            Console.WriteLine($"List Count: {firstTwoNumbers.Count}. Elements: {string.Join(", ", firstTwoNumbers)}");
+            var firstTwoNumbers = CopyAtMost(numbersList, 2);
+            DisplayListItems(firstTwoNumbers);
+
+            var firstTwoNames = CopyAtMost(namesList, 2);
+            DisplayListItems(firstTwoNames);
+
+            DisplayListItems(CopyAtMost(boolList, 3));
         }
 
-        public static List<T> CopyAtMost<T>(List<T> numbersList, int maxElements)
+        private static List<T> CopyAtMost<T>(List<T> inputList, int maxElements)
         {
-            var capacity = Math.Min(numbersList.Count, maxElements);
+            var capacity = Math.Min(inputList.Count, maxElements);
             List<T> returnList = new(capacity);
 
             for (int i = 0; i < capacity; i++)
             {
-                returnList.Add(numbersList[i]);
+                returnList.Add(inputList[i]);
             }
 
             return returnList;
+        }
+
+        private static void DisplayListItems<T>(List<T> inputList)
+        {
+            Console.WriteLine($"List Count: {inputList.Count}. Elements: {string.Join(", ", inputList)}");
         }
 
     }
