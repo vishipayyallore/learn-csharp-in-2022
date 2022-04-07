e008f695e49d1064550106a65ea9e8547dfbbc1a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-07 12:37:19 +0530

    Code Refactoring

---
+++ csharp-development-fundamentals/Source/Generics/SimpleStack.App/Helpers/ShowSimpleStackDemoV1.cs
@@ -0,0 +1,28 @@
+﻿using SimpleStack.Lib.V1;
+
+using static System.Console;
+
+namespace SimpleStack.App.Helpers
+{
+    public static class ShowSimpleStackDemoV1
+    {
+        public static void StackDoubles()
+        {
+            var stack = new SimpleStackDouble();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ csharp-development-fundamentals/Source/Generics/SimpleStack.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using SimpleStack.App.Helpers;
+using SimpleStack.Lib.V1;
+using static System.Console;
+
+ShowSimpleStackDemoV1.StackDoubles();
+StackStrings();
+
+WriteLine("\n\nPress any key ...");
+
+
+
+static void StackStrings()
+{
+    var stack = new SimpleStackString();
+    stack.Push("Pluralsight");
+    stack.Push("Udemy");
+    stack.Push("Cloud Academy");
+
+    while (stack.Count > 0)
+    {
+        Console.WriteLine(stack.Pop());
+    }
+}
