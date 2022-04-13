68533d81a17ca3e217b5847c0464421afd62fba4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-13 12:54:57 +0530

    First Application with DI

--- csharp-in-depth-4e/Source/ArrayDemos/ArrayDemos.Run/Program.cs
+++ csharp-in-depth-4e/Source/ArrayDemos/ArrayDemos.Run/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArrayDemos.Run.Apps;
+using ArrayDemos.Run.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+
+using static System.Console;
+
+// Generate a provider
+var serviceProvider = ConfigureNamesArrayDemoApp.ConfigureServices();
+
+Console.ResetColor();
+
+serviceProvider?.GetService<NamesArrayDemoApp>()?.Run();
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
