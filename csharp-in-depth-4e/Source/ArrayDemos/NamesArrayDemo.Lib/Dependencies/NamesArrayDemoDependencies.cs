0cc2321c0beee436eb1cc515618a732dd8cda2c2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-13 12:38:32 +0530

    Moved the Dependencies into its respective Libraries

--- csharp-in-depth-4e/Source/ArrayDemos/NamesArrayDemo.Lib/Dependencies/NamesArrayDemoDependencies.cs
+++ csharp-in-depth-4e/Source/ArrayDemos/NamesArrayDemo.Lib/Dependencies/NamesArrayDemoDependencies.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using NameGenerator.Lib;
-using NameGenerator.Lib.Interfaces;
 using NamesArrayDemo.Lib.Interfaces;
 
 namespace NamesArrayDemo.Lib.Dependencies
@@ -9,9 +7,8 @@ namespace NamesArrayDemo.Lib.Dependencies
     public static class NamesArrayDemoDependencies
     {
 
-        public static IServiceCollection ConfigureArrayDemoServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureNamesArrayDemoServices(this IServiceCollection services)
         {
-            _ = services.AddTransient<IGenerateName, GenerateName>();
             _ = services.AddTransient<INamesArray, NamesArray>();
             _ = services.AddTransient<IPrintHelper, PrintHelper>();
 
