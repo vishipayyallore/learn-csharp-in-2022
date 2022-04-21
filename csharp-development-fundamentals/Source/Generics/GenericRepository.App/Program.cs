a26344ee7ff650a439f9ee0747a4cea69283ee05
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-21 19:03:19 +0530

    Update Program.cs

--- csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
+++ csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
@@ -1,27 +1,36 @@
-﻿
-using GenericRepository.Lib.Entities;
+﻿using GenericRepository.Lib.Entities;
 using GenericRepository.Lib.Repositories.V1;
 using GenericRepository.Lib.Repositories.V2;
 
 AddEmployees(new EmployeeRepository());
-
 AddEmployeesV1(new GenericRepository<Employee>());
 
 static void AddEmployees(EmployeeRepository employeeRepository)
 {
-    employeeRepository.Add(new Employee { FirstName = "Sri Varu" });
-    employeeRepository.Add(new Employee { FirstName = "Vignesh" });
-    employeeRepository.Add(new Employee { FirstName = "RamaKrishna" });
+    foreach (var employee in GetAllEmployees())
+    {
+        employeeRepository.Add(employee);
+    }
 
     employeeRepository.Save();
 }
 
 static void AddEmployeesV1(GenericRepository<Employee> employeeRepository)
 {
-    employeeRepository.Add(new Employee { FirstName = "Sri Varu" });
-    employeeRepository.Add(new Employee { FirstName = "Vignesh" });
-    employeeRepository.Add(new Employee { FirstName = "RamaKrishna" });
+    foreach (var employee in GetAllEmployees())
+    {
+        employeeRepository.Add(employee);
+    }
 
     employeeRepository.Save();
 }
 
+static IEnumerable<Employee> GetAllEmployees()
+{
+    return new List<Employee>
+    {
+        new Employee { FirstName = "Sri Varu" },
+        new Employee { FirstName = "Vignesh" },
+        new Employee { FirstName = "RamaKrishna" }
+    };
+}
