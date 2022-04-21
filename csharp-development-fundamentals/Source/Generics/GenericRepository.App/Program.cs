c3879c2ddb481f8f19a7fe1ca1ef31f343b4f8c7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-21 18:52:41 +0530

    Initial Version

--- csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
+++ csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
@@ -1,14 +1,27 @@
 ﻿
 using GenericRepository.Lib.Entities;
 using GenericRepository.Lib.Repositories.V1;
+using GenericRepository.Lib.Repositories.V2;
 
 AddEmployees(new EmployeeRepository());
+
+AddEmployeesV1(new GenericRepository<Employee>());
 
 static void AddEmployees(EmployeeRepository employeeRepository)
 {
     employeeRepository.Add(new Employee { FirstName = "Sri Varu" });
     employeeRepository.Add(new Employee { FirstName = "Vignesh" });
     employeeRepository.Add(new Employee { FirstName = "RamaKrishna" });
+
+    employeeRepository.Save();
+}
+
+static void AddEmployeesV1(GenericRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Sri Varu" });
+    employeeRepository.Add(new Employee { FirstName = "Vignesh" });
+    employeeRepository.Add(new Employee { FirstName = "RamaKrishna" });
+
     employeeRepository.Save();
 }
 
