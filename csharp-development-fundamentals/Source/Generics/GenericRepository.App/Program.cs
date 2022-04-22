504e15339e1e31b5fb44d10ebccb4079ddc709ed
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-22 22:19:18 +0530

    Update Program.cs

--- csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
+++ csharp-development-fundamentals/Source/Generics/GenericRepository.App/Program.cs
@@ -4,6 +4,7 @@ using GenericRepository.Lib.Repositories.V2;
 
 AddEmployees(new EmployeeRepository());
 AddEmployeesV1(new GenericRepository<Employee>());
+AddOrganizations(new GenericRepository<Organization>());
 
 static void AddEmployees(EmployeeRepository employeeRepository)
 {
@@ -25,6 +26,16 @@ static void AddEmployeesV1(GenericRepository<Employee> employeeRepository)
     employeeRepository.Save();
 }
 
+static void AddOrganizations(GenericRepository<Organization> organizationRepository)
+{
+    foreach (var organization in GetAllOrganization())
+    {
+        organizationRepository.Add(organization);
+    }
+
+    organizationRepository.Save();
+}
+
 static IEnumerable<Employee> GetAllEmployees()
 {
     return new List<Employee>
@@ -32,5 +43,15 @@ static IEnumerable<Employee> GetAllEmployees()
         new Employee { FirstName = "Sri Varu" },
         new Employee { FirstName = "Vignesh" },
         new Employee { FirstName = "RamaKrishna" }
+    };
+}
+
+static IEnumerable<Organization> GetAllOrganization()
+{
+    return new List<Organization>
+    {
+        new Organization { Name = "Organization 1"},
+        new Organization { Name = "Organization 2"},
+        new Organization { Name = "Organization 3"},
     };
 }
