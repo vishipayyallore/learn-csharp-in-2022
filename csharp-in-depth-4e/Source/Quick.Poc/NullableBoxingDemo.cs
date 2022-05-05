c8101c8411d883bb27d4f71531bd62f770f29513
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-05 09:35:32 +0530

    Today's Program

---
+++ csharp-in-depth-4e/Source/Quick.Poc/NullableBoxingDemo.cs
@@ -0,0 +1,20 @@
+﻿namespace Quick.Poc
+{
+
+    public class NullableBoxingDemo
+    {
+
+        public static void ShowDemo()
+        {
+            int? noValue = new();
+            object noValueBoxed = noValue;
+            Console.WriteLine($"Original Value: {noValue} Value: {noValueBoxed} Type: {noValueBoxed.GetType()} Equals: {noValueBoxed == null}");
+
+            int? someValue = new int?(5);
+            object someValueBoxed = someValue;
+            Console.WriteLine($"Original Value: {someValue} Value: {someValueBoxed} Type: {someValueBoxed.GetType()} Equals: {someValueBoxed == null}");
+        }
+
+    }
+
+}
--- csharp-in-depth-4e/Source/Quick.Poc/Program.cs
+++ csharp-in-depth-4e/Source/Quick.Poc/Program.cs
@@ -2,6 +2,7 @@
 
 NamesWithStringCollection.ShowDemo();
 NamesWithList.ShowDemo();
+NullableBoxingDemo.ShowDemo();
 
 Console.WriteLine("\nHello World !");
 
