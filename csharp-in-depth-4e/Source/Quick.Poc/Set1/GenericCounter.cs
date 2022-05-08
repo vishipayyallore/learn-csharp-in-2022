42042e95788705eeed14bfa94925ca3e60a4ddf4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-08 13:04:39 +0530

    Moved the code into separate class

---
+++ csharp-in-depth-4e/Source/Quick.Poc/Set1/GenericCounter.cs
@@ -0,0 +1,23 @@
+﻿namespace Quick.Poc.Set1
+{
+    public class GenericCounter<T>
+    {
+        private static int value;
+
+        static GenericCounter()
+        {
+            Console.WriteLine("Initializing counter for {0}", typeof(T));
+        }
+
+        public static void Increment()
+        {
+            value++;
+        }
+
+        public static void Display()
+        {
+            Console.WriteLine("Counter for {0}: {1}", typeof(T), value);
+        }
+    }
+
+}
--- csharp-in-depth-4e/Source/Quick.Poc/Set1/GenericCounterDemo.cs
+++ csharp-in-depth-4e/Source/Quick.Poc/Set1/GenericCounterDemo.cs
@@ -1,39 +1,21 @@
 ﻿using Quick.Poc.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quick.Poc.Set1
 {
-    
+
     public class GenericCounterDemo : IDemoProgram
     {
+
         public void ShowDemo()
         {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class GenericCounter<T>
-    {
-        private static int value;
-
-        static GenericCounter()
-        {
-            Console.WriteLine("Initializing counter for {0}", typeof(T));
+            GenericCounter<string>.Increment();
+            GenericCounter<string>.Increment();
+            GenericCounter<string>.Display();
+            GenericCounter<int>.Display();
+            GenericCounter<int>.Increment();
+            GenericCounter<int>.Display();
         }
 
-        public static void Increment()
-        {
-            value++;
-        }
-
-        public static void Display()
-        {
-            Console.WriteLine("Counter for {0}: {1}", typeof(T), value);
-        }
     }
 
 }
