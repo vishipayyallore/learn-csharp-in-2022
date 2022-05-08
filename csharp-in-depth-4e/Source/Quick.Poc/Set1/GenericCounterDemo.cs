56487f6473c8dbd1bbffc0c686a7d3d13176276e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-08 10:30:50 +0530

    Create GenericCounterDemo.cs

---
+++ csharp-in-depth-4e/Source/Quick.Poc/Set1/GenericCounterDemo.cs
@@ -0,0 +1,39 @@
+﻿using Quick.Poc.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Poc.Set1
+{
+    
+    public class GenericCounterDemo : IDemoProgram
+    {
+        public void ShowDemo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenericCounter<T>
+    {
+        private static int value;
+
+        static GenericCounter()
+        {
+            Console.WriteLine("Initializing counter for {0}", typeof(T));
+        }
+
+        public static void Increment()
+        {
+            value++;
+        }
+
+        public static void Display()
+        {
+            Console.WriteLine("Counter for {0}: {1}", typeof(T), value);
+        }
+    }
+
+}
