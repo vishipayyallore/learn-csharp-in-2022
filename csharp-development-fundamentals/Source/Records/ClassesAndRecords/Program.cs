a149feb18a37112959728f28102595d7b06830fb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-07 11:46:12 +0530

    Initial Version

---
+++ csharp-development-fundamentals/Source/Records/ClassesAndRecords/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassesAndRecords.Entities;
+
+using static System.Console;
+
+var pluralsightCourse = new Course();
+pluralsightCourse.Name = "Working with C# Records";
+pluralsightCourse.Author = "Some Author";
+
+var classroomCourse = new CourseRecord(
+    "Working with C# Records",
+    "Some Author"
+    );
+
+// Not Allowed
+// classroomCourse.Name = "Another title";
+
+WriteLine("\nPress any key ...");
