155a6910b404440754ce386d14472b437ee586ac
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-15 13:50:50 +0530

    Moved the Repo to separate class and folder

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/NullObject/LearnersDemo/Repositories/LearnerRepo.cs
@@ -0,0 +1,40 @@
+﻿using LearnersDemo.Entities;
+
+namespace LearnersDemo.Services
+{
+
+    public class LearnerRepo : ILearnerRepo
+    {
+        private static readonly IList<Learner> _learners = new List<Learner>();
+
+        internal LearnerRepo()
+        {
+            _learners.Add(GetNewLearner(1, "David", 83));
+
+            _learners.Add(GetNewLearner(2, "Julie", 72));
+
+            _learners.Add(GetNewLearner(3, "Scott", 92));
+        }
+
+        private static Learner GetNewLearner(int id, string userName, int coursesCompleted)
+        {
+            return new Learner(id, userName, coursesCompleted);
+        }
+
+        public ILearner GetLearner(int id)
+        {
+            ILearner learner = new NullLearner();
+
+            bool learnerExists = _learners.Any(l => l.Id == id);
+
+            if (learnerExists)
+            {
+                learner = _learners.FirstOrDefault(l => l.Id == id)!;
+            }
+
+            return learner;
+        }
+
+    }
+
+}
--- learn-designpatterns-in-csharp/Source/Behavioral/NullObject/LearnersDemo/Services/LearnerService.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/NullObject/LearnersDemo/Services/LearnerService.cs
@@ -16,39 +16,6 @@ namespace LearnersDemo.Services
             return _repo.GetLearner(learnerId);
         }
 
-        class LearnerRepo
-        {
-            readonly IList<Learner> _learners = new List<Learner>();
-
-            internal LearnerRepo()
-            {
-                _learners.Add(GetNewLearner(1, "David", 83));
-
-                _learners.Add(GetNewLearner(2, "Julie", 72));
-
-                _learners.Add(GetNewLearner(3, "Scott", 92));
-            }
-
-            private static Learner GetNewLearner(int id, string userName, int coursesCompleted)
-            {
-                return new Learner(id, userName, coursesCompleted);
-            }
-
-            internal ILearner GetLearner(int id)
-            {
-                ILearner learner = new NullLearner();
-
-                bool learnerExists = _learners.Any(l => l.Id == id);
-
-                if (learnerExists)
-                {
-                    learner = _learners.FirstOrDefault(l => l.Id == id)!;
-                }
-
-                return learner;
-            }
-        }
-
     }
 
 }
