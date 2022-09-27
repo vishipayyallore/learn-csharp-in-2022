5f730c27b2d2cbacf72ad0e0de6dcf72dd8da9fc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-27 10:05:40 +0530

    Create Logger.cs

---
+++ learn-designpatterns-in-csharp/Source/Creational/Singleton/LoggerDemo/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using static System.Console;
+
+namespace LoggerDemo
+{
+
+    public sealed class Logger
+    {
+
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+            }
+        }
+
+        public void Log(string message)
+        {
+            WriteLine($"Message to log: {message}");
+        }
+
+    }
+
+}
