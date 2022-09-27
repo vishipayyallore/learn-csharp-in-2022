14470bd51c1cb21f3a6b1005dd59514af6b32b71
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-27 10:14:54 +0530

    Swamy/25sep2022 names arraydemo (#9)
    * Initial version
    
    * Few Changes
    
    * Folder Reorganization
    
    * Singleton Pattern
    
    * Create Logger.cs
    
    * Basic Implementation
    
    * Update Program.cs

---
+++ learn-designpatterns-in-csharp/Source/Creational/Singleton/LoggerDemo/Logger.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+namespace LoggerDemo
+{
+
+    public sealed class Logger
+    {
+
+        private static readonly Lazy<Logger> _lazyLogger = new(() => new Logger());
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+            }
+        }
+
+        public void Log(string message)
+        {
+            WriteLine($"Message to log: {message}");
+        }
+
+    }
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Creational/Singleton/LoggerDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using LoggerDemo;
+using static System.Console;
+
+Title = "Singleton";
+
+// call the property getter twice
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+{
+    ForegroundColor = ConsoleColor.Green;
+    WriteLine("Instances are the SAME.");
+}
+else
+{
+    ForegroundColor = ConsoleColor.Red;
+    WriteLine("Instances are the DIFFERENT.");
+}
+
+ForegroundColor = ConsoleColor.Cyan;
+instance1.Log($"Message from {nameof(instance1)}");
+instance1.Log($"Message from {nameof(instance2)}");
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
