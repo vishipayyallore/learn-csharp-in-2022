d5a9ee348fbd427f919e4f349c606988db3b27d3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-27 10:13:48 +0530

    Update Program.cs

--- learn-designpatterns-in-csharp/Source/Creational/Singleton/LoggerDemo/Program.cs
+++ learn-designpatterns-in-csharp/Source/Creational/Singleton/LoggerDemo/Program.cs
@@ -1,7 +1,7 @@
 ﻿using LoggerDemo;
 using static System.Console;
 
-Console.Title = "Singleton";
+Title = "Singleton";
 
 // call the property getter twice
 var instance1 = Logger.Instance;
@@ -9,16 +9,20 @@ var instance2 = Logger.Instance;
 
 if (instance1 == instance2 && instance2 == Logger.Instance)
 {
-    Console.WriteLine("Instances are the SAME.");
+    ForegroundColor = ConsoleColor.Green;
+    WriteLine("Instances are the SAME.");
 }
 else
 {
-    Console.WriteLine("Instances are the DIFFERENT.");
+    ForegroundColor = ConsoleColor.Red;
+    WriteLine("Instances are the DIFFERENT.");
 }
 
+ForegroundColor = ConsoleColor.Cyan;
 instance1.Log($"Message from {nameof(instance1)}");
 instance1.Log($"Message from {nameof(instance2)}");
 Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
 
+ResetColor();
 WriteLine("\n\nPress any key ...");
 ReadKey();
