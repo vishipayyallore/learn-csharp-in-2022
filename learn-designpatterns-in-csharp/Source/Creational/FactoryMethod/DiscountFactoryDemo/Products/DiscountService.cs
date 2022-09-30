b0c823b9b834daeafdd78b7cb8a3cca3fe5f0ecf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-30 13:10:14 +0530

    Initial Version

---
+++ learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Products/DiscountService.cs
@@ -0,0 +1,55 @@
+﻿namespace DiscountFactoryDemo.Products
+{
+
+    /// <summary>
+    /// Product
+    /// </summary>
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+
+        public override string ToString() => GetType().Name;
+    }
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// ConcretCreator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+}
