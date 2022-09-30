a7032cdf99d642e00a84878d609195fa7450b4fd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-30 13:11:43 +0530

    Concrete Creators

---
+++ learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Creators/DiscountFactory.cs
@@ -0,0 +1,48 @@
+﻿using DiscountFactoryDemo.Products;
+
+namespace DiscountFactoryDemo.Creators
+{
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// ConcretCreator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+
+}
--- learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Products/DiscountService.cs
+++ learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Products/DiscountService.cs
@@ -11,45 +11,4 @@
         public override string ToString() => GetType().Name;
     }
 
-    /// <summary>
-    /// Creator
-    /// </summary>
-    public abstract class DiscountFactory
-    {
-        public abstract DiscountService CreateDiscountService();
-    }
-
-    /// <summary>
-    /// ConcretCreator
-    /// </summary>
-    public class CountryDiscountFactory : DiscountFactory
-    {
-        private readonly string _countryIdentifier;
-        public CountryDiscountFactory(string countryIdentifier)
-        {
-            _countryIdentifier = countryIdentifier;
-        }
-
-        public override DiscountService CreateDiscountService()
-        {
-            return new CountryDiscountService(_countryIdentifier);
-        }
-    }
-
-    /// <summary>
-    /// ConcreteCreator
-    /// </summary>
-    public class CodeDiscountFactory : DiscountFactory
-    {
-        private readonly Guid _code;
-
-        public CodeDiscountFactory(Guid code)
-        {
-            _code = code;
-        }
-        public override DiscountService CreateDiscountService()
-        {
-            return new CodeDiscountService(_code);
-        }
-    }
 }
