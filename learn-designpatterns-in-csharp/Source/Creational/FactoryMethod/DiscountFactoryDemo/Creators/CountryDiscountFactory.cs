8942b0f4b871aadbd1665944bc2896d73f3d83d9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-30 13:12:24 +0530

    Code Refactor

---
+++ learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Creators/CountryDiscountFactory.cs
@@ -0,0 +1,22 @@
+﻿using DiscountFactoryDemo.Products;
+
+namespace DiscountFactoryDemo.Creators
+{
+    /// <summary>
+    /// ConcretCreator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+}
--- learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Creators/DiscountFactory.cs
+++ learn-designpatterns-in-csharp/Source/Creational/FactoryMethod/DiscountFactoryDemo/Creators/DiscountFactory.cs
@@ -11,38 +11,4 @@ namespace DiscountFactoryDemo.Creators
         public abstract DiscountService CreateDiscountService();
     }
 
-    /// <summary>
-    /// ConcretCreator
-    /// </summary>
-    public class CountryDiscountFactory : DiscountFactory
-    {
-        private readonly string _countryIdentifier;
-        public CountryDiscountFactory(string countryIdentifier)
-        {
-            _countryIdentifier = countryIdentifier;
-        }
-
-        public override DiscountService CreateDiscountService()
-        {
-            return new CountryDiscountService(_countryIdentifier);
-        }
-    }
-
-    /// <summary>
-    /// ConcreteCreator
-    /// </summary>
-    public class CodeDiscountFactory : DiscountFactory
-    {
-        private readonly Guid _code;
-
-        public CodeDiscountFactory(Guid code)
-        {
-            _code = code;
-        }
-        public override DiscountService CreateDiscountService()
-        {
-            return new CodeDiscountService(_code);
-        }
-    }
-
 }
