959f8b8b4abe901e06b6e77b740bd1394559ad21
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-08 17:45:46 +0530

    Data Models

---
+++ learn-designpatterns-in-csharp/Source/Structural/Adapter/CityAdapter.Data/CityFromExternalSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace CityAdapter.Data
+{
+
+    public class CityFromExternalSystem
+    {
+        public string Name { get; private set; }
+        public string NickName { get; private set; }
+        public int Inhabitants { get; private set; }
+
+        public CityFromExternalSystem(string name, string nickName, int inhabitants)
+        {
+            Name = name;
+
+            NickName = nickName;
+
+            Inhabitants = inhabitants;
+        }
+
+    }
+
+}
