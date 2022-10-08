5aeda487f74e4e0246efb8243d310ef792afaed5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-08 18:06:51 +0530

    Class Adapter Verified

---
+++ learn-designpatterns-in-csharp/Source/Structural/Adapter/CityClassAdapterDemo/Adapters/CityAdapter.cs
@@ -0,0 +1,25 @@
+﻿using CityAdapterData;
+using CityClassAdapterDemo.Adaptees;
+using CityClassAdapterDemo.Targets;
+
+namespace CityClassAdapterDemo.Adapters
+{
+
+    /// <summary>
+    /// Adapter
+    /// </summary>
+    public class CityAdapter : ExternalSystem, ICityAdapter
+    {
+        public City GetCity()
+        {
+            // call into the external system 
+            var cityFromExternalSystem = base.GetCity();
+
+            // adapt the CityFromExternalCity to a City 
+            return new City(
+                $"{cityFromExternalSystem.Name} - {cityFromExternalSystem.NickName}"
+                , cityFromExternalSystem.Inhabitants);
+        }
+    }
+
+}
--- learn-designpatterns-in-csharp/Source/Structural/Adapter/CityClassAdapterDemo/Program.cs
+++ learn-designpatterns-in-csharp/Source/Structural/Adapter/CityClassAdapterDemo/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CityClassAdapterDemo.Adapters;
+using CityClassAdapterDemo.Targets;
+using static System.Console;
+
+Title = "Design Pattern - Class Adapter";
+
+ForegroundColor = ConsoleColor.Cyan;
+
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+
+Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
