a505bf1fb92ccb7d42e666db1cdb46ee1fd3baf7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-17 13:50:08 +0530

    Abstraction

---
+++ learn-designpatterns-in-csharp/Source/Structural/Bridge/FoodMenuDemo/Abstraction/MeatBasedMenu.cs
@@ -0,0 +1,19 @@
+﻿using FoodMenuDemo.Implementor;
+
+namespace FoodMenuDemo.Abstraction
+{
+    /// <summary>
+    /// RefinedAbstraction
+    /// </summary>
+    public class MeatBasedMenu : FoodMenu
+    {
+        public MeatBasedMenu(ICoupon coupon) : base(coupon)
+        {
+        }
+        public override int CalculatePrice()
+        {
+            return 30 - _coupon.CouponValue;
+        }
+    }
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Structural/Bridge/FoodMenuDemo/Abstraction/VegetarianMenu.cs
@@ -0,0 +1,19 @@
+﻿using FoodMenuDemo.Implementor;
+
+namespace FoodMenuDemo.Abstraction
+{
+    /// <summary>
+    /// RefinedAbstraction
+    /// </summary>
+    public class VegetarianMenu : FoodMenu
+    {
+        public VegetarianMenu(ICoupon coupon) : base(coupon)
+        {
+        }
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+
+}
