a184bcacb83bba0e42d38cec8b6bdbc540b3299e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-18 12:15:08 +0530

    Decorators

---
+++ learn-designpatterns-in-csharp/Source/Structural/Decorator/MailServiceDemo/Decorators/IMailService.cs
@@ -0,0 +1,11 @@
+﻿namespace MailServiceDemo.Decorators
+{
+
+    /// <summary>
+    /// Component (as interface)
+    /// </summary>
+    public interface IMailService
+    {
+        bool SendMail(string message);
+    }
+}
---
+++ learn-designpatterns-in-csharp/Source/Structural/Decorator/MailServiceDemo/Decorators/OnPremiseMailService.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace MailServiceDemo.Decorators
+{
+    /// <summary>
+    /// ConcreteComponent2
+    /// </summary>
+    public class OnPremiseMailService : IMailService
+    {
+
+        public bool SendMail(string message)
+        {
+            WriteLine($"Message \"{message}\" sent via {nameof(OnPremiseMailService)}.");
+
+            return true;
+        }
+
+    }
+}
