7849efd0bde7da7edb39e9b40f810e85167c8a77
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-18 12:19:33 +0530

    Decorators

---
+++ learn-designpatterns-in-csharp/Source/Structural/Decorator/MailServiceDemo/Decorators/MailServiceDecoratorBase.cs
@@ -0,0 +1,24 @@
+﻿using MailServiceDemo.Components;
+
+namespace MailServiceDemo.Decorators
+{
+
+    /// <summary>
+    /// Decorator (as abstract base class)
+    /// </summary>
+    public abstract class MailServiceDecoratorBase : IMailService
+    {
+        private readonly IMailService _mailService;
+
+        public MailServiceDecoratorBase(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public virtual bool SendMail(string message)
+        {
+            return _mailService.SendMail(message);
+        }
+    }
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Structural/Decorator/MailServiceDemo/Decorators/StatisticsDecorator.cs
@@ -0,0 +1,25 @@
+﻿using MailServiceDemo.Components;
+
+using static System.Console;
+
+namespace MailServiceDemo.Decorators
+{
+    /// <summary>
+    /// ConcreteDecorator1
+    /// </summary>
+    public class StatisticsDecorator : MailServiceDecoratorBase
+    {
+        public StatisticsDecorator(IMailService mailService) : base(mailService)
+        {
+        }
+
+        public override bool SendMail(string message)
+        {
+            // Fake collecting statistics 
+            WriteLine($"Collecting statistics in {nameof(StatisticsDecorator)}.");
+
+            return base.SendMail(message);
+        }
+    }
+
+}
