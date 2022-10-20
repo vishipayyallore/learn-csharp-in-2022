e6aa37f056055cf19023f5515a768a61f97b8341
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-20 10:34:44 +0530

    Verified Facade

---
+++ learn-designpatterns-in-csharp/Source/Structural/Facade/OrderServiceDemo/Facades/DiscountFacade.cs
@@ -0,0 +1,27 @@
+﻿using OrderServiceDemo.Subsystems;
+
+namespace OrderServiceDemo.Facades
+{
+
+    /// <summary>
+    /// Facade
+    /// </summary>
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountBaseService _customerDiscountBaseService = new();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscountPercentage(int customerId)
+        {
+            if (!_orderService.HasEnoughOrders(customerId))
+            {
+                return 0;
+            }
+
+            return _customerDiscountBaseService.CalculateDiscountBase(customerId)
+                * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
+
+}
