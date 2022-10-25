a425b3960f0c24f6c67a872fbf22d3c9bcd3669e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-25 13:27:45 +0530

    Folder Structure

--- learn-designpatterns-in-csharp/Source/Structural/Flyweight/CharactersDemo/Program.cs
+++ learn-designpatterns-in-csharp/Source/Structural/Flyweight/CharactersDemo/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Title = "Design Pattern - Flyweight";
+
+
+ForegroundColor = ConsoleColor.Cyan;
+
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
