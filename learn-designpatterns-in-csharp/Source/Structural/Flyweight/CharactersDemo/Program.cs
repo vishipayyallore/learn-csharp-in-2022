e1edcfa205cc129a27117e9b411357824011f6bd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-26 14:26:49 +0530

    Update Program.cs

--- learn-designpatterns-in-csharp/Source/Structural/Flyweight/CharactersDemo/Program.cs
+++ learn-designpatterns-in-csharp/Source/Structural/Flyweight/CharactersDemo/Program.cs
@@ -1,10 +1,34 @@
-﻿using static System.Console;
+﻿using CharactersDemo.FlyweightFactory;
+using CharactersDemo.Flyweights;
+using static System.Console;
 
 Title = "Design Pattern - Flyweight";
 
-
 ForegroundColor = ConsoleColor.Cyan;
 
+var aBunchOfCharacters = "abba";
+
+var characterFactory = new CharacterFactory();
+
+// Get the flyweight(s)
+var characterObject = characterFactory.GetCharacter(aBunchOfCharacters[0]);
+// Pass through extrinsic state
+characterObject?.Draw("Arial", 12);
+
+characterObject = characterFactory.GetCharacter(aBunchOfCharacters[1]);
+characterObject?.Draw("Trebuchet MS", 14);
+
+characterObject = characterFactory.GetCharacter(aBunchOfCharacters[2]);
+characterObject?.Draw("Times New Roman", 16);
+
+characterObject = characterFactory.GetCharacter(aBunchOfCharacters[3]);
+characterObject?.Draw("Comic Sans", 18);
+
+// create unshared concrete flyweight (paragraph)
+var paragraph = characterFactory.CreateParagraph(new List<ICharacter>() { characterObject! }, 1);
+
+// draw the paragraph
+paragraph.Draw("Lucinda", 12);
 
 ResetColor();
 WriteLine("\n\nPress any key ...");
