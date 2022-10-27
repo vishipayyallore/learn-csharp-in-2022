2e5220d3f1f2d0835c8dd99c2b064f0e283b0a74
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-27 13:19:26 +0530

    Initial Version

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/TemplateMethod/MailParserDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+Title = "Design Pattern - TemplateMethod";
+
+ForegroundColor = ConsoleColor.Cyan;
+
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
