a2722cea162d37ed3c6c55ed118958176aad848d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-27 15:53:25 +0530

    Classes

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/TemplateMethod/MailParserDemo/AbstractTemplateMethods/MailParser.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace MailParserDemo.AbstractTemplateMethods
+{
+
+    /// <summary>
+    /// AbstractClass
+    /// </summary>
+    public abstract class MailParser
+    {
+        public virtual void FindServer()
+        {
+            WriteLine("Finding server...");
+        }
+
+        public abstract void AuthenticateToServer();
+
+
+        public virtual string ParseHtmlMailBody(string identifier)
+        {
+            WriteLine("Parsing HTML mail body...");
+            return $"This is the body of mail with id {identifier}";
+        }
+
+        /// <summary>
+        /// Template method
+        /// </summary> 
+        public string ParseMailBody(string identifier)
+        {
+            WriteLine("Parsing mail body (in template method)...");
+            FindServer();
+            AuthenticateToServer();
+            return ParseHtmlMailBody(identifier);
+        }
+    }
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Behavioral/TemplateMethod/MailParserDemo/TemplateMethods/ApacheMailParser.cs
@@ -0,0 +1,13 @@
+﻿using MailParserDemo.AbstractTemplateMethods;
+
+namespace MailParserDemo.TemplateMethods
+{
+    public class ApacheMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Apache");
+        }
+    }
+
+}
