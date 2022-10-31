99a4b2d7dc712f96a50551f2826125528f0161a6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-31 11:14:15 +0530

    Files

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Memento/EmployeeManagerDemo/Mementos/AddEmployeeToManagerListMemento.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagerDemo.Commands;
+
+namespace EmployeeManagerDemo.Mementos
+{
+    /// <summary>
+    /// Memento
+    /// </summary>
+    public class AddEmployeeToManagerListMemento
+    {
+        public int ManagerId { get; private set; }
+        public Employee? Employee { get; private set; }
+
+        public AddEmployeeToManagerListMemento(int managerId, Employee? employee)
+        {
+            ManagerId = managerId;
+            Employee = employee;
+        }
+    }
+
+}
