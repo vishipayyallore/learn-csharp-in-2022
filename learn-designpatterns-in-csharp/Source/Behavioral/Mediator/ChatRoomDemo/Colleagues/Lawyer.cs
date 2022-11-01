36e5964c88cf040af04f650a4fff442299e6a9e0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-01 15:16:04 +0530

    Created Classes

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Mediator/ChatRoomDemo/Colleagues/Lawyer.cs
@@ -0,0 +1,21 @@
+﻿namespace ChatRoomDemo.Colleagues
+{
+    /// <summary>
+    /// ConcreteColleague
+    /// </summary>
+    public class Lawyer : TeamMember
+    {
+        private List<TeamMember> _teamMembersInChat = new();
+
+        public Lawyer(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.Write($"{nameof(Lawyer)} {Name} received: ");
+            base.Receive(from, message);
+        }
+    }
+
+}
