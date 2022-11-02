b6dcee9fa561fd398c9a30554bd66af79cb6922d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-02 13:05:07 +0530

    Handlers

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/ChainOfResponsibility/DocumentDemo/Handlers/IHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace DocumentDemo.Handlers
+{
+
+    /// <summary>
+    /// Handler
+    /// </summary> 
+    public interface IHandler<T> where T : class
+    {
+        IHandler<T> SetSuccessor(IHandler<T> successor);
+
+        void Handle(T request);
+    }
+
+
+}
