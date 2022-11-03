d0d6e9e6ec53f7f4e7910fb0be4a88fccfd02086
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 10:21:56 +0530

    Observers

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Observer/TrainTicketsDemo/Observers/TicketStockService.cs
@@ -0,0 +1,20 @@
+﻿using TrainTicketsDemo.Models;
+
+using static System.Console;
+
+namespace TrainTicketsDemo.Observers
+{
+    /// <summary>
+    /// ConcreteObserver
+    /// </summary>
+    public class TicketStockService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            // update local datastore (datastore omitted in demo implementation)
+            WriteLine($"{nameof(TicketStockService)} notified of ticket change: artist {ticketChange.ArtistId}, amount {ticketChange.Amount}");
+        }
+    }
+
+
+}
