57312b0fb2986e3b26563dff4cfb5f87e1550d29
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 10:14:38 +0530

    Create TicketChange.cs

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Observer/TrainTicketsDemo/Models/TicketChange.cs
@@ -0,0 +1,18 @@
+﻿namespace TrainTicketsDemo.Models
+{
+
+    public class TicketChange
+    {
+        public int Amount { get; private set; }
+
+        public int ArtistId { get; private set; }
+
+        public TicketChange(int artistId, int amount)
+        {
+            ArtistId = artistId;
+
+            Amount = amount;
+        }
+    }
+
+}
