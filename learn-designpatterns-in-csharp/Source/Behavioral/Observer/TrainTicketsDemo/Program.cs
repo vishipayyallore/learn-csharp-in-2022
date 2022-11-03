f4fd0c8d3f9457e2793679171f55474029c19c94
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-03 10:23:51 +0530

    Observer Verified

--- learn-designpatterns-in-csharp/Source/Behavioral/Observer/TrainTicketsDemo/Program.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/Observer/TrainTicketsDemo/Program.cs
@@ -1,8 +1,27 @@
-﻿using static System.Console;
+﻿using TrainTicketsDemo.Observers;
+using TrainTicketsDemo.Subjects;
+using static System.Console;
 
 Title = "Design Pattern - Observer";
 
 ForegroundColor = ConsoleColor.Cyan;
+
+TicketStockService ticketStockService = new();
+TicketResellerService ticketResellerService = new();
+OrderService orderService = new();
+
+// add two observers
+orderService.AddObserver(ticketStockService);
+orderService.AddObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(1, 2);
+
+// remove one observer
+orderService.RemoveObserver(ticketResellerService);
+
+// notify
+orderService.CompleteTicketSale(2, 4);
 
 ResetColor();
 WriteLine("\n\nPress any key ...");
