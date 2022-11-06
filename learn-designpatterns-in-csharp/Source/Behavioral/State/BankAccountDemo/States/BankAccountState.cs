4a2838910fe3c6d5a0cddd66a55fec64b1156896
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-06 09:45:40 +0530

    Code Refactoring

--- learn-designpatterns-in-csharp/Source/Behavioral/State/BankAccountDemo/States/BankAccountState.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/State/BankAccountDemo/States/BankAccountState.cs
@@ -17,40 +17,4 @@ namespace BankAccountDemo.States
         public abstract void Withdraw(decimal amount);
     }
 
-    /// <summary>
-    /// ConcreteState
-    /// </summary>
-    public class OverdrawnState : BankAccountState
-    {
-        public OverdrawnState(decimal balance, BankAccount bankAccount)
-        {
-            Balance = balance;
-            BankAccount = bankAccount;
-        }
-
-        /// <summary>
-        /// Handle a deposit
-        /// </summary> 
-        public override void Deposit(decimal amount)
-        {
-            Console.WriteLine($"In {GetType()}, depositing {amount}");
-            Balance += amount;
-            if (Balance >= 0)
-            {
-                // change state to regular
-                BankAccount.BankAccountState = new RegularState(Balance, BankAccount);
-            }
-        }
-
-        /// <summary>
-        /// Handle withdrawing
-        /// </summary> 
-        public override void Withdraw(decimal amount)
-        {
-            // cannot withdraw
-            Console.WriteLine($"In {GetType()}, cannot withdraw, balance {Balance}");
-        }
-    }
-
-
 }
