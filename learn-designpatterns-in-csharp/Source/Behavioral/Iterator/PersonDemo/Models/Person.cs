7922c1283e5c2f7518a38a1904f1b8724925cbb2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-06 10:00:04 +0530

    Model Class

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Iterator/PersonDemo/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonDemo.Models
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public Person(string name, string country)
+        {
+            Name = name;
+            Country = country;
+        }
+    }
+
+}
