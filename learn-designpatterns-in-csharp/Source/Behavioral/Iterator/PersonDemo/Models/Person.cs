e69891ea415b7b25a9c7385132a98aeb61b71d99
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-06 10:01:53 +0530

    Interface

--- learn-designpatterns-in-csharp/Source/Behavioral/Iterator/PersonDemo/Models/Person.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/Iterator/PersonDemo/Models/Person.cs
@@ -4,11 +4,13 @@
     public class Person
     {
         public string Name { get; set; }
+
         public string Country { get; set; }
 
         public Person(string name, string country)
         {
             Name = name;
+            
             Country = country;
         }
     }
