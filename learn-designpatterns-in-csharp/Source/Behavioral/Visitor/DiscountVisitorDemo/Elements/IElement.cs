8709bae0273c9f7c290925b602e230a28dec2f1f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-07 10:04:05 +0530

    Interfaces

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Visitor/DiscountVisitorDemo/Elements/IElement.cs
@@ -0,0 +1,19 @@
+﻿using DiscountVisitorDemo.Visitors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountVisitorDemo.Elements
+{
+
+    /// <summary>
+    /// Element
+    /// </summary>
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Visitor/DiscountVisitorDemo/Visitors/IVisitor.cs
@@ -0,0 +1,13 @@
+﻿namespace DiscountVisitorDemo.Visitors
+{
+
+    /// <summary>
+    /// Visitor
+    /// </summary>
+    public interface IVisitor
+    {
+        void VisitCustomer(Customer customer);
+        void VisitEmployee(Employee employee);
+    }
+
+}
