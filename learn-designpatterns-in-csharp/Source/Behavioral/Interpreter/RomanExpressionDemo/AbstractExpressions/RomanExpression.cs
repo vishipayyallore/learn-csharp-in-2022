d54cc7374683f0c845cc3de4f361099659761452
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-09 12:57:08 +0530

    Added Classes

---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemo/AbstractExpressions/RomanExpression.cs
@@ -0,0 +1,35 @@
+﻿using RomanExpressionDemo.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanExpressionDemo.AbstractExpressions
+{
+
+    /// <summary>
+    /// AbstractExpression
+    /// </summary>
+    public abstract class RomanExpression
+    {
+        public abstract void Interpret(RomanContext value);
+    }
+
+    // 9 = IX
+    // 8 = VIII
+    // 7 = VII
+    // 6 = VI
+    // 5 = V
+    // 4 = IV
+    // 3 = III
+    // 2 = II
+    // 1 = I
+
+    // simplified - each combination is reachable with substraction and these 4:
+    // 9 = IX 
+    // 5 = V
+    // 4 = IV
+    // 1 = I
+
+}
---
+++ learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemo/TerminalExpressions/RomanOneExpression.cs
@@ -0,0 +1,61 @@
+﻿using RomanExpressionDemo.AbstractExpressions;
+using RomanExpressionDemo.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanExpressionDemo.TerminalExpressions
+{
+
+    // 9 = IX
+    // 8 = VIII
+    // 7 = VII
+    // 6 = VI
+    // 5 = V
+    // 4 = IV
+    // 3 = III
+    // 2 = II
+    // 1 = I
+
+    // simplified - each combination is reachable with substraction and these 4:
+    // 9 = IX 
+    // 5 = V
+    // 4 = IV
+    // 1 = I
+
+    /// <summary>
+    /// TerminalExpression
+    /// </summary>
+    public class RomanOneExpression : RomanExpression
+    {
+        public override void Interpret(RomanContext value)
+        {
+            while ((value.Input - 9) >= 0)
+            {
+                value.Output += "IX";
+                value.Input -= 9;
+            }
+
+            while ((value.Input - 5) >= 0)
+            {
+                value.Output += "V";
+                value.Input -= 5;
+            }
+
+            while ((value.Input - 4) >= 0)
+            {
+                value.Output += "IV";
+                value.Input -= 4;
+            }
+
+            while ((value.Input - 1) >= 0)
+            {
+                value.Output += "I";
+                value.Input -= 1;
+            }
+        }
+    }
+
+}
