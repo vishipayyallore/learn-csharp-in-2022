509d2a753eba1157e3bcd8cbe8bd1eb20014292e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-10 13:12:51 +0530

    Update RomanHunderdExpression.cs

--- learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/TerminalExpressions/RomanHunderdExpression.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/TerminalExpressions/RomanHunderdExpression.cs
@@ -11,30 +11,48 @@ namespace RomanExpressionDemoV1.TerminalExpressions
     {
         public override void Interpret(RomanContext value)
         {
-            while (value.Input - 900 >= 0)
-            {
-                value.Output += "CM";
-                value.Input -= 900;
-            }
+            //while (value.Input - 900 >= 0)
+            //{
+            //    value.Output += "CM";
+            //    value.Input -= 900;
+            //}
 
-            while (value.Input - 500 >= 0)
-            {
-                value.Output += "D";
-                value.Input -= 500;
-            }
+            //while (value.Input - 500 >= 0)
+            //{
+            //    value.Output += "D";
+            //    value.Input -= 500;
+            //}
 
-            while (value.Input - 400 >= 0)
-            {
-                value.Output += "CD";
-                value.Input -= 400;
-            }
+            //while (value.Input - 400 >= 0)
+            //{
+            //    value.Output += "CD";
+            //    value.Input -= 400;
+            //}
 
-            while (value.Input - 100 >= 0)
+            //while (value.Input - 100 >= 0)
+            //{
+            //    value.Output += "C";
+            //    value.Input -= 100;
+            //}
+
+            CalculateValue(value, 900, "CM");
+
+            CalculateValue(value, 500, "D");
+
+            CalculateValue(value, 400, "CD");
+
+            CalculateValue(value, 100, "C");
+        }
+
+        private static void CalculateValue(RomanContext value, int numberValue, string romanValue)
+        {
+            while (value.Input - numberValue >= 0)
             {
-                value.Output += "C";
-                value.Input -= 100;
+                value.Output += romanValue;
+                value.Input -= numberValue;
             }
         }
+
     }
 
 }
