8a6c100cdb8ef2aabb9d42817f7c794fc886ded4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-10 12:58:01 +0530

    Program.cs updates

--- learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/Program.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Title = "Design Pattern - Interpreter";
+
+ForegroundColor = ConsoleColor.Cyan;
+
+//var expressions = new List<RomanExpression>
+//{
+//    new RomanHunderdExpression(),
+//    new RomanTenExpression(),
+//    new RomanOneExpression(),
+//};
+
+//var context = new RomanContext(5);
+//foreach (var expression in expressions)
+//{
+//    expression.Interpret(context);
+//}
+//WriteLine($"Translating Arabic numerals to Roman numerals: 5 = {context.Output}");
+
+//context = new RomanContext(81);
+//foreach (var expression in expressions)
+//{
+//    expression.Interpret(context);
+//}
+//WriteLine($"Translating Arabic numerals to Roman numerals: 81 = {context.Output}");
+
+//context = new RomanContext(733);
+//foreach (var expression in expressions)
+//{
+//    expression.Interpret(context);
+//}
+//WriteLine($"Translating Arabic numerals to Roman numerals: 733 = {context.Output}");
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
+
