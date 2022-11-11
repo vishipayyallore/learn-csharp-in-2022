f509b69e680205998e83c92462f89ba20bf2bb76
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-11 10:16:27 +0530

    Update RomanHunderdExpression.cs

--- learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/TerminalExpressions/RomanHunderdExpression.cs
+++ learn-designpatterns-in-csharp/Source/Behavioral/Interpreter/RomanExpressionDemoV1/TerminalExpressions/RomanHunderdExpression.cs
@@ -11,30 +11,6 @@ namespace RomanExpressionDemoV1.TerminalExpressions
     {
         public override void Interpret(RomanContext value)
         {
-            //while (value.Input - 900 >= 0)
-            //{
-            //    value.Output += "CM";
-            //    value.Input -= 900;
-            //}
-
-            //while (value.Input - 500 >= 0)
-            //{
-            //    value.Output += "D";
-            //    value.Input -= 500;
-            //}
-
-            //while (value.Input - 400 >= 0)
-            //{
-            //    value.Output += "CD";
-            //    value.Input -= 400;
-            //}
-
-            //while (value.Input - 100 >= 0)
-            //{
-            //    value.Output += "C";
-            //    value.Input -= 100;
-            //}
-
             InterpretValue(value, 900, "CM");
 
             InterpretValue(value, 500, "D");
@@ -43,15 +19,6 @@ namespace RomanExpressionDemoV1.TerminalExpressions
 
             InterpretValue(value, 100, "C");
         }
-
-        //private static void CalculateValue(RomanContext value, int numberValue, string romanValue)
-        //{
-        //    while (value.Input - numberValue >= 0)
-        //    {
-        //        value.Output += romanValue;
-        //        value.Input -= numberValue;
-        //    }
-        //}
 
     }
 
